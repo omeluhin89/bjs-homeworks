ea28c0d84b13efef136e9eb014a6c744abca2fab
omeluhin89 <omeluhin89@gmail.com>
2019-10-28 01:49:52 +0500

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -10,7 +10,42 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    if (typeof percent === 'string' || typeof percent === "number") {
+        percent = Number(percent);
+    } else {
+        console.log(`Параметр процентной ставки содержит неправильное значение ${percent}`);
+        return
+    }
+    if (typeof contribution === 'string' || typeof contribution === "number") {
+        contribution = Number(contribution)
+    } else {
+        console.log(`Параметр процентной ставки содержит неправильное значение ${contribution}`);
+        return
+    }
+    if (typeof amount === 'string' || typeof amount === "number") {
+        amount = Number(amount);
+    } else {
+        console.log(`Параметр процентной ставки содержит неправильное значение ${amount}`);
+        return
+    }
+    if (typeof date !== 'string' && typeof date !== "number") {
+        console.log(`Параметр процентной ставки содержит неправильное значение ${date}`);
+        return
+    }
+    let dateEndCredit = +new Date(date);
+    const dateNow = Date.now();
+    let quantityMonth = ((dateEndCredit - dateNow) / (3600 * 24 * 1000 * 30)).toFixed();
 
+    //let diff = new Date(date);
+    console.log(quantityMonth);
+
+    const sumReturnBank = Number(amount) - Number(contribution);
+    const P = (percent/100) / 12;
+    const pay = sumReturnBank * (P + P / (((1 + P) ** quantityMonth) -1));
+    const totalSum = contribution + sumReturnBank + (pay * quantityMonth) - amount;
+    const totalAmount = Number(totalSum.toFixed(2));
+    console.log(totalAmount);
+    return totalAmount;
     // код для задачи №1 писать здесь
     //return totalAmount;
 }
@@ -23,6 +58,13 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    let greeting;
+    if (Boolean(name)){
+        greeting = `Привет, мир! Меня зовут ${name}`;
+        console.log(greeting);
+    } else {
+        greeting = `Привет, мир! Меня зовут Аноним`;
+        console.log(greeting);
+    }
+    return greeting
 }
