c95c1d0985101b5993c6734255867c2d1e151aae
omeluhin89 <omeluhin89@gmail.com>
2019-10-27 22:57:30 +0500

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,18 @@
+/*Задание 1*/
+const algebra  = 5;
+const geography = 4;
+const physics = 4;
+const averageValue = (algebra + geography + physics) / 3;
+console.log(averageValue);
+
+/*Задание 2*/
+const name = 'Ярослав';
+const message = `Привет, мир! Меня зовут ${name}`;
+console.log(message);
+
+/*Задание 3*/
+const x = 2;
+const y = 22;
+const z = 0;
+const result = x * y + 5 * z + x - 1;
+console.log(result);
